5e22ee61bc3e1414c18d9ea1be764e8d8d9a7a5e
Manikandan S <153970783+smaneee9769@users.noreply.github.com>
2023-12-16 10:19:37 +0530

    Add files via upload

---
+++ count.js
@@ -0,0 +1,25 @@
+var counterContainer = document.querySelector(".website-counter");
+var counterpost = document.querySelector(".website");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+website.addEventListener("click", () => {
+    if (visitCount) {
+        visitCount = Number(visitCount) + 1;
+        localStorage.setItem("page_view", visitCount);
+      } else {
+        visitCount = 1;
+        localStorage.setItem("page_view", 1);
+      }
+      counterpost.innerHTML = visitCount;
+});
